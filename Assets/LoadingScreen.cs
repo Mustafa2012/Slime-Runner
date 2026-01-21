dc2652cf275502edcee9bdcb21b104cbfe0cb950
Syed Mustafa Ali <mustafaali192012@gmail.com>
2026-01-22 01:12:44 +0500

    Made some changes

--- Assets/LoadingScreen.cs
+++ Assets/LoadingScreen.cs
@@ -1,1 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
+public class LoadingScreen : MonoBehaviour
+{
+    public string sceneToLoad = "GameScene";  
+    public float minimumTime = 3f;            
+
+    void Start()
+    {
+        StartCoroutine(LoadGame());
+    }
+
+    private IEnumerator LoadGame()
+    {
+        yield return new WaitForSeconds(minimumTime);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
